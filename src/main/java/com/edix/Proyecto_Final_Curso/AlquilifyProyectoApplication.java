4085ad36d400780c50fb3ebf3e09b24a119aeb6f
SabryCat <85496805+SabryCat@users.noreply.github.com>
2023-04-27 20:46:20 +0200

    Carga de proyecto

---
+++ src/main/java/com/edix/Proyecto_Final_Curso/AlquilifyProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.edix.Proyecto_Final_Curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilifyProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilifyProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edix/Proyecto_Final_Curso/AlquilifyProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edix.Proyecto_Final_Curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilifyProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
